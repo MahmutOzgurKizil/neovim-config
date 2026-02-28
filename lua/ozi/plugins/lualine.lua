0cbe2aa8b0d7a15841642ffa690ac18df326c9f3
MahmutOzgurKizil <ozgurkizil@hotmail.com>
2026-02-28 15:53:42 +0300

    Add diagnostic messages to lualine

--- lua/ozi/plugins/lualine.lua
+++ lua/ozi/plugins/lualine.lua
@@ -1,101 +1,32 @@
 return {
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { { 'nvim-tree/nvim-web-devicons', opt = false } },
+        dependencies = {{
+            'nvim-tree/nvim-web-devicons',
+            'Isrothy/lualine-diagnostic-message',
+            opt = false
+        }},
         config = function()
-            local utils = require("lualine.utils.utils")
-            local highlight = require("lualine.highlight")
-
-            local diagnostics_message = require("lualine.component"):extend()
-
-            diagnostics_message.default = {
-                colors = {
-                    error = utils.extract_color_from_hllist(
-                        { "fg", "sp" },
-                        { "DiagnosticError", "LspDiagnosticsDefaultError", "DiffDelete" },
-                        "#e32636"
-                    ),
-                    warning = utils.extract_color_from_hllist(
-                        { "fg", "sp" },
-                        { "DiagnosticWarn", "LspDiagnosticsDefaultWarning", "DiffText" },
-                        "#ffa500"
-                    ),
-                    info = utils.extract_color_from_hllist(
-                        { "fg", "sp" },
-                        { "DiagnosticInfo", "LspDiagnosticsDefaultInformation", "DiffChange" },
-                        "#ffffff"
-                    ),
-                    hint = utils.extract_color_from_hllist(
-                        { "fg", "sp" },
-                        { "DiagnosticHint", "LspDiagnosticsDefaultHint", "DiffAdd" },
-                        "#273faf"
-                    ),
-                },
-            }
-
-            function diagnostics_message:init(options)
-                diagnostics_message.super:init(options)
-                self.options.colors = vim.tbl_extend("force", diagnostics_message.default.colors, self.options.colors or {})
-                self.highlights = { error = "", warn = "", info = "", hint = "" }
-                self.highlights.error = highlight.create_component_highlight_group(
-                    { fg = self.options.colors.error },
-                    "diagnostics_message_error",
-                    self.options
-                )
-                self.highlights.warn = highlight.create_component_highlight_group(
-                    { fg = self.options.colors.warn },
-                    "diagnostics_message_warn",
-                    self.options
-                )
-                self.highlights.info = highlight.create_component_highlight_group(
-                    { fg = self.options.colors.info },
-                    "diagnostics_message_info",
-                    self.options
-                )
-                self.highlights.hint = highlight.create_component_highlight_group(
-                    { fg = self.options.colors.hint },
-                    "diagnostics_message_hint",
-                    self.options
-                )
-            end
-
-            function diagnostics_message:update_status()
-                local r, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
-                local diagnostics = vim.diagnostic.get(0, { lnum = r - 1 })
-                if #diagnostics > 0 then
-                    local diag = diagnostics[1]
-                    for _, d in ipairs(diagnostics) do
-                        if d.severity < diag.severity then
-                            diag = d
-                        end
-                    end
-                    local icons = { '', '', '', '' }
-                    local hl = { self.highlights.error, self.highlights.warn, self.highlights.info, self.highlights.hint }
-                    return highlight.component_format_highlight(hl[diag.severity]) .. icons[diag.severity] .. " " .. diag.message
-                else
-                    return ""
-                end
-            end
-
             require('lualine').setup({
-                options = {
-                    theme = 'iceberg_dark',
-                    icons_enabled = true,
-                    component_separators = { left = '', right = '' },
-                    section_separators = { left = '', right = '' },
-                },
                 sections = {
                     lualine_c = {
                         {
-                            diagnostics_message,
+                            "diagnostic-message",
                             colors = {
                                 error = "#BF616A",
                                 warn = "#EBCB8B",
                                 info = "#A3BE8C",
                                 hint = "#88C0D0",
                             },
+                            first_line_only = true,
                         },
-                    },
+                    }
+                },
+                options = {
+                    theme = 'iceberg_dark',
+                    icons_enabled = true,
+                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
                 },
             })
         end
--- lua/ozi/plugins/treesitter.lua
+++ lua/ozi/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = 'nvim-treesitter.config',
+    main = 'nvim-treesitter.configs',
     config = function()
         require('nvim-treesitter').setup {
             -- A list of parser names
             ensure_installed = { "rust", "c", "java", "vim", "lua" },
             sync_install = false,
             auto_install = true,
-
+            indent = { enable = true, },
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
